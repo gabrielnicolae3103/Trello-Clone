803965e932ff84ec85c8217973adf8f2928f96af
Gabriel Nicolae <gabriel.nicolae313@gmail.com>
2020-05-13 17:29:46 +0300

    card styling

--- client/trello-clone/src/components/Card.vue
+++ client/trello-clone/src/components/Card.vue
@@ -1,9 +1,31 @@
 <template>
 	<div>
-		<b-button v-b-modal="cardTitle">Show Modal</b-button>
+		<b-button v-b-modal='card._id'>Show Modal</b-button>
 
 			<!-- The modal -->
-			<b-modal :id='cardTitle'>{{card.description}}{{card.title}}</b-modal>
+			<b-modal :title='card.title' :id='card._id'>
+				<label for="title-input-live">Title:</label>
+				<b-form-input
+					id="title-input-live"
+					v-model="card.title"
+					:state="titleState"
+					aria-describedby="input-live-help input-live-feedback"
+					placeholder="Enter title"
+					trim
+				></b-form-input>
+				<label for="description-input-live">Description:</label>
+				<b-form-input
+					id="description-input-live"
+					v-model="card.description"
+					:state="descriptionState"
+					aria-describedby="input-live-help input-live-feedback"
+					placeholder="Enter description"
+					trim
+				></b-form-input>
+				<b-form-invalid-feedback id="input-live-feedback">
+					Enter at least 3 letters
+				</b-form-invalid-feedback>
+			</b-modal>
 	</div>
 </template>
 
@@ -12,11 +34,14 @@ export default {
 	name: 'Card',
 	props: ['card'],
 	data: () => ({
-		cardTitle: ''
 	}),
-	created: function() {
-		this.cardTitle = this.card.title;
-	}
-
+	computed: {
+		titleState() {
+			return this.card.title.length > 2 ? true : false
+		},
+		descriptionState() {
+			return this.card.description.length > 2 ? true : false
+		}
+	},
 }
 </script>
